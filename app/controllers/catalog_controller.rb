dfd11cf1c7e850eee8c776c260b8842752be550c
Mike Kanning <mikekanning@gmail.com>
2017-08-23 16:44:12 -0400

    copy facet, index and show config from Meta CatalogController, including I18n values and helpers.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,146 +4,114 @@ class CatalogController < ApplicationController
   include Blacklight::Catalog
 
   configure_blacklight do |config|
-    ## Class for sending and receiving requests from a search index
-    # config.repository_class = Blacklight::Solr::Repository
-    #
-    ## Class for converting Blacklight's url parameters to into request parameters for the search index
-    # config.search_builder_class = ::SearchBuilder
-    #
-    ## Model that maps search index responses to the blacklight response model
-    # config.response_model = Blacklight::Solr::Response
 
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
-      rows: 10
+        qt: 'search',
+        fq: '-class_name:Repository +portal_names_sms:"The Digital Library of Georgia"'
     }
 
-    # solr path which will be added to solr base url before the other solr params.
-    #config.solr_path = 'select'
+    # facets are defined in query handler so this is not needed
+    # makes debugging solr calls easier
+    config.add_facet_fields_to_solr_request!
+
+    # set maximum results per page (experimental)
+    config.max_per_page = 20000
 
     # items to show per page, each number in the array represent another option to choose from.
-    #config.per_page = [10,20,50,100]
-
-    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SearchHelper#solr_doc_params) or
-    ## parameters included in the Blacklight-jetty document requestHandler.
-    #
-    #config.default_document_solr_params = {
-    #  qt: 'document',
-    #  ## These are hard-coded in the blacklight 'document' requestHandler
-    #  # fl: '*',
-    #  # rows: 1,
-    #  # q: '{!term f=id v=$id}'
-    #}
+    config.per_page = [20,50,100,1000]
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_display'
+    config.index.title_field = 'title'
     config.index.display_type_field = 'format'
-    #config.index.thumbnail_field = 'thumbnail_path_ss'
 
     # solr field configuration for document/show views
-    #config.show.title_field = 'title_display'
-    #config.show.display_type_field = 'format'
-    #config.show.thumbnail_field = 'thumbnail_path_ss'
+    config.show.title_field = 'title'
+    config.show.display_type_field = 'format'
 
-    # solr fields that will be treated as facets by the blacklight application
-    #   The ordering of the field names is the order of the display
-    #
-    # Setting a limit will trigger Blacklight's 'more' facet values link.
-    # * If left unset, then all facet values returned by solr will be displayed.
-    # * If set to an integer, then "f.somefield.facet.limit" will be added to
-    # solr request, with actual solr request being +1 your configured limit --
-    # you configure the number of items you actually want _displayed_ in a page.
-    # * If set to 'true', then no additional parameters will be sent to solr,
-    # but any 'sniffed' request limit parameters will be used for paging, with
-    # paging at requested limit -1. Can sniff from facet.limit or
-    # f.specific_field.facet.limit solr request params. This 'true' config
-    # can be used if you set limits in :default_solr_params, or as defaults
-    # on the solr side in the request handler itself. Request handler defaults
-    # sniffing requires solr requests to be made with "echoParams=all", for
-    # app code to actually have it echo'd back to see it.
-    #
-    # :show may be set to false if you don't want the facet to be drawn in the
-    # facet bar
-    #
-    # set :index_range to true if you want the facet pagination view to have facet prefix-based navigation
-    #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
-    # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
+    # show thumbnails on search results for most view types
+    # config.index.thumbnail_field = :thumbnail_url
+    config.index.thumbnail_method = :record_thumbnail
 
-    config.add_facet_field 'format', label: 'Format'
-
-    # Have BL send all facet field names to Solr, which has been the default
-    # previously. Simply remove these lines if you'd rather use Solr request
-    # handler defaults, or have no facets.
-    config.add_facet_fields_to_solr_request!
+    # FACETS
+    config.add_facet_field 'provenance_facet',        label: I18n.t('search.facets.provenance'), limit: true
+    config.add_facet_field 'publisher_facet',         label: I18n.t('search.facets.publisher'), limit: true
+    config.add_facet_field 'creator_facet',           label: I18n.t('search.facets.creator'), limit: true
+    config.add_facet_field 'contributor_facet',       label: I18n.t('search.facets.contributor'), limit: true
+    config.add_facet_field 'subject_facet',           label: I18n.t('search.facets.subject'), limit: true
+    config.add_facet_field 'subject_personal_facet',  label: I18n.t('search.facets.subject_personal'), limit: true
+    config.add_facet_field 'year_facet',              label: I18n.t('search.facets.year'), limit: true
+    config.add_facet_field 'temporal_facet',          label: I18n.t('search.facets.temporal'), limit: true
+    config.add_facet_field 'location_facet',          label: I18n.t('search.facets.location'), limit: true
+    config.add_facet_field 'format_facet',            label: I18n.t('search.facets.format'), limit: true
+    config.add_facet_field 'rights_facet',            label: I18n.t('search.facets.rights'), limit: true, helper_method: :rights_icon_label
+    config.add_facet_field 'rights_holder_facet'    , label: I18n.t('search.facets.rights_holder'), limit: true
+    config.add_facet_field 'relation_facet',          label: I18n.t('search.facets.relation'), limit: true
+    config.add_facet_field 'type_facet',              label: I18n.t('search.facets.type'), limit: true
+    config.add_facet_field 'medium_facet',            label: I18n.t('search.facets.medium'), limit: true
+    config.add_facet_field 'language_facet',          label: I18n.t('search.facets.language'), limit: true
+    config.add_facet_field 'repository_name_sms',     label: I18n.t('search.facets.repository'), limit: true
+    config.add_facet_field 'collection_name_sms',     label: I18n.t('search.facets.collection'), limit: true
+    config.add_facet_field 'class_name',              label: I18n.t('search.facets.class'), limit: true
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'title_display', label: 'Title'
+    config.add_index_field 'record_id_ss',                label: I18n.t('search.labels.record_id')
+    config.add_index_field 'dcterms_title_display',       label: I18n.t('search.labels.dcterms_title')
+    config.add_index_field 'dcterms_description_display', label: I18n.t('search.labels.dcterms_description'), helper_method: 'truncate_index'
+    config.add_index_field 'collection_name_sms',         label: I18n.t('search.labels.collection'), link_to_search: true
+    config.add_index_field 'repository_name_sms',         label: I18n.t('search.labels.repository'), link_to_search: true
+    config.add_index_field 'dcterms_identifier_display',  label: I18n.t('search.labels.dcterms_identifier')
+    config.add_index_field 'edm_is_shown_at_display',     label: I18n.t('search.labels.edm_is_shown_at'), helper_method: 'linkify'
+    config.add_index_field 'edm_is_shown_by_display',     label: I18n.t('search.labels.edm_is_shown_by'), helper_method: 'linkify'
+    config.add_index_field 'dcterms_creator_display',     label: I18n.t('search.labels.dcterms_creator'), link_to_search: :creator_facet
+    config.add_index_field 'dc_format_display',           label: I18n.t('search.labels.dc_format'), link_to_search: :format_facet
+    config.add_index_field 'dcterms_spatial_display',     label: I18n.t('search.labels.dcterms_spatial'), link_to_search: :location_facet
+
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_display', label: 'Title'
-    config.add_show_field 'title_vern_display', label: 'Title'
-    config.add_show_field 'subtitle_display', label: 'Subtitle'
-    config.add_show_field 'subtitle_vern_display', label: 'Subtitle'
-    config.add_show_field 'author_display', label: 'Author'
-    config.add_show_field 'author_vern_display', label: 'Author'
-    config.add_show_field 'format', label: 'Format'
-    config.add_show_field 'url_fulltext_display', label: 'URL'
-    config.add_show_field 'url_suppl_display', label: 'More Information'
-    config.add_show_field 'language_facet', label: 'Language'
-    config.add_show_field 'published_display', label: 'Published'
-    config.add_show_field 'published_vern_display', label: 'Published'
-    config.add_show_field 'lc_callnum_display', label: 'Call number'
-    config.add_show_field 'isbn_t', label: 'ISBN'
-
-    # "fielded" search configuration. Used by pulldown among other places.
-    # For supported keys in hash, see rdoc for Blacklight::SearchFields
-    #
-    # Search fields will inherit the :qt solr request handler from
-    # config[:default_solr_parameters], OR can specify a different one
-    # with a :qt key/value. Below examples inherit, except for subject
-    # that specifies the same :qt as default for our own internal
-    # testing purposes.
-    #
-    # The :key is what will be used to identify this BL search field internally,
-    # as well as in URLs -- so changing it after deployment may break bookmarked
-    # urls.  A display label will be automatically calculated from the :key,
-    # or can be specified manually to be different.
-
-    # This one uses all the defaults set by the solr request handler. Which
-    # solr request handler? The one set in config[:default_solr_parameters][:qt],
-    # since we aren't specifying it otherwise.
-
-    config.add_search_field 'all_fields', label: 'All Fields'
-
-
-    # Now we see how to over-ride Solr request handler defaults, in this
-    # case for a BL "search field", which is really a dismax aggregate
-    # of Solr search fields.
-
-    config.add_search_field('title') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
-
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = {
-        qf: '$title_qf',
-        pf: '$title_pf'
-      }
-    end
-
+    config.add_show_field 'slug_ss',                                label: I18n.t('search.labels.item_id')
+    config.add_show_field 'record_id_ss',                           label: I18n.t('search.labels.record_id')
+    config.add_show_field 'dcterms_title_display',                  label: I18n.t('search.labels.dcterms_title')
+    config.add_show_field 'collection_name_sms',                    label: I18n.t('search.labels.collection'), link_to_search: true
+    config.add_show_field 'repository_name_sms',                    label: I18n.t('search.labels.repository'), link_to_search: true
+    config.add_show_field 'dcterms_is_part_of_display',             label: I18n.t('search.labels.dcterms_is_part_of')
+    config.add_show_field 'dcterms_description_display',            label: I18n.t('search.labels.dcterms_description')
+    config.add_show_field 'dc_format_display',                      label: I18n.t('search.labels.dc_format')
+    config.add_show_field 'dcterms_identifier_display',             label: I18n.t('search.labels.dcterms_identifier')
+    config.add_show_field 'dc_right_display',                       label: I18n.t('search.labels.dc_right'), helper_method: 'rights_icon_tag'
+    config.add_show_field 'dc_date_display',                        label: I18n.t('search.labels.dc_date')
+    config.add_show_field 'dc_relation_display',                    label: I18n.t('search.labels.dc_relation')
+    config.add_show_field 'dcterms_publisher_display',              label: I18n.t('search.labels.dcterms_publisher')
+    config.add_show_field 'dcterms_contributor_display',            label: I18n.t('search.labels.dcterms_contributor')
+    config.add_show_field 'dcterms_temporal_display',               label: I18n.t('search.labels.dcterms_temporal')
+    config.add_show_field 'dcterms_spatial_display',                label: I18n.t('search.labels.dcterms_spatial'), link_to_search: :location_facet
+    config.add_show_field 'dcterms_provenance_display',             label: I18n.t('search.labels.dcterms_provenance')
+    config.add_show_field 'dcterms_subject_display',                label: I18n.t('search.labels.dcterms_subject'), link_to_search: :subject_facet
+    config.add_show_field 'dcterms_type_display',                   label: I18n.t('search.labels.dcterms_type')
+    config.add_show_field 'dcterms_creator_display',                label: I18n.t('search.labels.dcterms_creator'), link_to_search: :creator_facet
+    config.add_show_field 'dcterms_language_display',               label: I18n.t('search.labels.dcterms_language')
+    config.add_show_field 'edm_is_shown_at_display',                label: I18n.t('search.labels.edm_is_shown_at'), helper_method: 'linkify'
+    config.add_show_field 'edm_is_shown_by_display',                label: I18n.t('search.labels.edm_is_shown_by'), helper_method: 'linkify'
+    config.add_show_field 'dcterms_rights_holder_display',          label: I18n.t('search.labels.dcterms_rights_holder')
+    config.add_show_field 'dcterms_bibliographic_citation_display', label: I18n.t('search.labels.dcterms_bibliographic_citation')
+    config.add_show_field 'dcterms_extent_display',                 label: I18n.t('search.labels.dcterms_extent')
+    config.add_show_field 'dcterms_medium_display',                 label: I18n.t('search.labels.dcterms_medium')
+    config.add_show_field 'dlg_subject_personal_display',           label: I18n.t('search.labels.dlg_subject_personal')
+    config.add_show_field 'created_at_dts',                         label: I18n.t('search.labels.created_at')
+    config.add_show_field 'updated_at_dts',                         label: I18n.t('search.labels.updated_at')
 
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', label: 'relevance'
-    config.add_sort_field 'pub_date_sort desc, title_sort asc', label: 'year'
-    config.add_sort_field 'author_sort asc, title_sort asc', label: 'author'
-    config.add_sort_field 'title_sort asc, pub_date_sort desc', label: 'title'
+    config.add_sort_field 'score desc, title_sort asc', label: 'Relevance'
+    config.add_sort_field 'year asc', label: 'Year'
+    config.add_sort_field 'title_sort asc', label: 'DC Title'
+    config.add_sort_field 'collection_sort asc', label: 'Collection'
+    config.add_sort_field 'creator_sort asc', label: 'DC Creator'
+    config.add_sort_field 'created_at_dts desc', label: 'Latest Created'
+    config.add_sort_field 'updated_at_dts desc', label: 'Latest Updated'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
---
+++ app/helpers/catalog_helper.rb
@@ -0,0 +1,52 @@
+module CatalogHelper
+
+  include Blacklight::CatalogHelperBehavior
+
+  INDEX_TRUNCATION_VALUE = 2500
+  NO_THUMB_ICON = 'no_thumb.gif' # todo replace with no thumb placeholder
+
+  def boolean_facet_labels(value)
+    value == 'true' ? 'Yes' : 'No'
+  end
+
+  # handle linking in catalog results
+  def linkify(options = {})
+    url = options[:value].first
+    link_to url, url
+  end
+
+  # truncate when displaying search results
+  def truncate_index(options = {})
+    truncate(
+        options[:value].join(I18n.t('support.array.two_words_connector')),
+        length: INDEX_TRUNCATION_VALUE,
+        omission: I18n.t('meta.search.index.truncated_field')
+    )
+  end
+
+  def rights_icon_label(uri)
+    I18n.t(:rights).each do |r|
+      return r[1][:label] if r[1][:uri] == uri
+    end
+    uri # return uri if no match found
+  end
+
+  def record_thumbnail(document, options)
+    url = case document['sunspot_id_ss'].split(' ')[0]
+            when 'Item'
+              if document.has_key?('slug_ss') &&
+                  document.has_key?('collection_slug_ss') &&
+                  document.has_key?('repository_slug_ss')
+                "http://dlg.galileo.usg.edu/#{document['repository_slug_ss']}/#{document['collection_slug_ss']}/do-th:#{document['slug_ss']}"
+              else
+                NO_THUMB_ICON
+              end
+            when 'Collection'
+              document['thumbnail_ss']
+            else
+              NO_THUMB_ICON
+          end
+    image_tag(url, class: 'thumbnail')
+  end
+
+end
