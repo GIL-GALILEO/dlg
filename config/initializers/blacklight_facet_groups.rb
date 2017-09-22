3a0b9cb1b6edd87e721d1e5b225a3697aed8f746
Mike Kanning <mikekanning@gmail.com>
2017-09-22 07:40:05 -0400

    put initializer code in a to_prepare block so it is reloaded as expected in development

--- config/initializers/blacklight_facet_groups.rb
+++ config/initializers/blacklight_facet_groups.rb
@@ -3,14 +3,16 @@
 # add Blacklight Facet helpers to support facet groups
 # see unmerged blacklight PR @
 # https://github.com/projectblacklight/blacklight/pull/1651
-Blacklight::Facet.module_eval do
-  def facet_field_names(group = nil)
-    blacklight_config.facet_fields.select do |_, opts|
-      group == opts[:group]
-    end.values.map(&:field)
-  end
+Rails.application.config.to_prepare do
+  Blacklight::Facet.module_eval do
+    def facet_field_names(group = nil)
+      blacklight_config.facet_fields.select do |_, opts|
+        group == opts[:group]
+      end.values.map(&:field)
+    end
 
-  def facet_group_names
-    blacklight_config.facet_fields.map { |_, opts| opts[:group] }.uniq
+    def facet_group_names
+      blacklight_config.facet_fields.map { |_, opts| opts[:group] }.uniq
+    end
   end
 end
