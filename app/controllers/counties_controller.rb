366f92255ff2bde2b46c0ff6a4b5325c67910424
Mike Kanning <mikekanning@gmail.com>
2017-10-04 16:28:35 -0400

    draw georgia counties on leaflet map. closes #28. closes #30.

--- app/controllers/counties_controller.rb
+++ app/controllers/counties_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class CountiesController < RecordsController
   COUNTIES_FACET_FIELD = 'counties_facet'
-  # include Blacklight::SearchHelper
+
   def index
     @counties = counties
     respond_to do |format|
@@ -12,26 +12,23 @@ class CountiesController < RecordsController
 
   private
 
-  # def search_builder
-  #   SearchBuilder.new(RecordsController)
-  # end
-
   def counties
-    response = get_facet_field_response(COUNTIES_FACET_FIELD)
-    counties_with_counts = response['facet_counts']['facet_fields'][COUNTIES_FACET_FIELD]
+    counties_with_counts = counties_facet_values
     counties = []
     counties_with_counts.each_with_index do |c, i|
-      counties << county_object(c, counties_with_counts[i + 1]) if c.is_a? String
+      next unless c.is_a? String
+      county = County.find_by_name c
+      next unless county
+      county.count = counties_with_counts[i + 1]
+      county.href = search_action_path(search_state.add_facet_params_and_redirect(COUNTIES_FACET_FIELD, c))
+      counties << county
     end
     counties
   end
 
-  def county_object(name, count)
-    OpenStruct.new(
-      name: name,
-      count: count,
-      link: search_action_path(search_state.add_facet_params_and_redirect(COUNTIES_FACET_FIELD, name))
-    )
+  def counties_facet_values
+    response = get_facet_field_response(COUNTIES_FACET_FIELD)
+    response['facet_counts']['facet_fields'][COUNTIES_FACET_FIELD]
   end
 
   # return link to faceted main results page
