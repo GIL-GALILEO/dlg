f8bc38d54b39110ca0f8c594ec0f4dc0d4feaeee
Mike Kanning <mikekanning@gmail.com>
2018-03-05 18:21:53 -0500

    remove obsolete helper specs, fix another

--- spec/helpers/catalog_helper_spec.rb
+++ spec/helpers/catalog_helper_spec.rb
@@ -36,35 +36,16 @@ RSpec.describe CatalogHelper do
 
   describe '#record_thumbnail' do
     context 'for an Item' do
-      it 'returns a thumbnail value containing a URL in do-th format' do
-        item_doc = {
-          'sunspot_id_ss' => 'Item 1',
-          'slug_ss' => 'item1',
-          'collection_slug_ss' => 'coll1',
-          'repository_slug_ss' => 'repo1'
-        }
-        expect(record_thumbnail(item_doc, {})).to(
-        include 'http://dlg.galileo.usg.edu/repo1/coll1/do-th:item1'
-        )
-      end
+
     end
     context 'for a Collection' do
-      it 'returns a thumbnail value from the document' do
-        item_doc = {
-          'sunspot_id_ss' => 'Collection 1',
-          'thumbnail_ss' => 'http://dlg.galileo.usg.edu/thumb.jpg'
-        }
-        expect(record_thumbnail(item_doc, {})).to(
-          include 'http://dlg.galileo.usg.edu/thumb.jpg'
-        )
-      end
     end
   end
 
   describe '#spatial_cleaner' do
-    it 'returns a string devoid of coordinates' do
+    it 'returns a string devoid of coordinates and "United States"' do
       value = 'United States, Georgia, Clarke County, Athens, 33.960948, -83.3779358'
-      expect(spatial_cleaner(value)).to eq 'United States, Georgia, Clarke County, Athens'
+      expect(spatial_cleaner(value)).to eq 'Georgia, Clarke County, Athens'
     end
   end
 
