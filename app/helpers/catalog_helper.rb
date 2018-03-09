4bbcc545d3bbc142ec7b0f3352247c52700d7eb2
Mike Kanning <mikekanning@gmail.com>
2018-03-09 15:14:50 -0500

    use repo images for collection thumbnails, if available. use generic icon for sound recordings.

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -127,11 +127,16 @@ module CatalogHelper
 
   # show thumbnail for item, or placeholder if none found
   def thumbnail_image_tag(document)
+    # return audio_recording_icon if is_audio_recording?(document)
     url = case document['class_name_ss']
           when 'Item'
-            "http://dlg.galileo.usg.edu/#{document['record_id_ss'].split('_')[0]}/#{document['record_id_ss'].split('_')[1]}/do-th:#{document['record_id_ss'].split('_')[2]}"
+            if document['dcterms_type_display'] == ['Sound']
+              '<i class="far fa-file-audio"></i>'
+            else
+              "http://dlg.galileo.usg.edu/#{document['record_id_ss'].split('_')[0]}/#{document['record_id_ss'].split('_')[1]}/do-th:#{document['record_id_ss'].split('_')[2]}"
+            end
           when 'Collection'
-            document['thumbnail_ss']
+            collection_thumb_url document
           else
             NO_THUMB_ICON
           end
@@ -202,4 +207,12 @@ module CatalogHelper
     document['record_id_ss']
   end
 
+  def collection_thumb_url(document)
+    # TODO INTERIM
+    if document['image_ss'] == "/dlg_default_image.png"
+      document['thumbnail_ss']
+    else
+      cache_server_image_link document['image_ss']
+    end
+  end
 end
