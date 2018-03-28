de7ecd0090549bbf1cfaffbf747237a46ac3c47a
Mike Kanning <mikekanning@gmail.com>
2018-03-28 15:35:50 -0400

    feature specs for search pages. fixes.

--- spec/features/collections_feature_spec.rb
+++ spec/features/collections_feature_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 feature 'Collections' do
-
+  before(:each) { visit search_collections_path }
+  context 'search bar' do
+    scenario 'uses collection search URL' do
+      expect(page).to have_css("form[action='#{search_collections_url}']")
+    end
+    scenario 'has a placeholder indicating it searches Collections' do
+      expect(page).to have_css("input[placeholder='#{I18n.t('search.bar.placeholder.collections')}']")
+    end
+  end
+  context 'results area' do
+    scenario 'defaults to list view' do
+      expect(page).to have_css('a.view-type-list.active')
+    end
+  end
 end
--- spec/features/records_feature_spec.rb
+++ spec/features/records_feature_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 feature 'Records' do
-
+  before(:each) { visit search_records_path }
+  context 'search bar' do
+    scenario 'uses records search URL' do
+      expect(page).to have_css("form[action='#{search_records_url}']")
+    end
+    scenario 'has a placeholder indicating it searches Collections' do
+      expect(page).to have_css("input[placeholder='#{I18n.t('search.bar.placeholder.default')}']")
+    end
+  end
+  context 'results area' do
+    scenario 'defaults to gallery view' do
+      expect(page).to have_css('a.view-type-gallery.active')
+    end
+  end
 end
