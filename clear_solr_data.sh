132c1e0330a105d133916e6ca0ff04f93e133807
Mike Kanning <mikekanning@gmail.com>
2018-04-11 11:07:01 -0400

    updates to sample/test data

--- clear_solr_data.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl 'http://localhost:6983/solr/meta-dev/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
-curl 'http://localhost:6983/solr/meta-test/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
--- reload_solr_data.sh
+++ reload_solr_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-curl 'http://localhost:8983/solr/meta-dev/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
-curl 'http://localhost:8983/solr/meta-test/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
+curl 'http://localhost:6983/solr/meta-dev/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
+curl 'http://localhost:6983/solr/meta-test/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
 
 curl 'http://localhost:6983/solr/meta-dev/update?commit=true' --data-binary '@provision/data.json' -H 'Content-type:application/json'
 curl 'http://localhost:6983/solr/meta-test/update?commit=true' --data-binary '@provision/data.json' -H 'Content-type:application/json'
