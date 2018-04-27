dda2d82e297b0e2b8658157d2b45e8b960353c76
Mike Kanning <mikekanning@gmail.com>
2018-04-27 11:10:36 -0400

    add slack as exception notifier. closes #118.

---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'exception_notification/rails'
+
+ExceptionNotification.configure do |config|
+  config.ignore_if do |_, _|
+    Rails.env.test? || Rails.env.development?
+  end
+  config.add_notifier :slack,
+                      webhook_url: Rails.application.secrets.slack_webhook_url,
+                      channel: '#app-exceptions',
+                      username: 'dlg-public',
+                      additional_parameters: {
+                        icon_emoji: ':boom:',
+                        mrkdwn: true
+                      }
+end
