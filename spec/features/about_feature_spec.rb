f4fbb74e8b2d48de8057e1d0fdd1e89c701c3047
Seamus Narron <seamus@uga.edu>
2019-02-14 15:03:52 -0500

    Filled in pending tests in about and participate feature specs

--- spec/features/about_feature_spec.rb
+++ spec/features/about_feature_spec.rb
@@ -5,30 +5,26 @@ require 'rails_helper'
 feature 'About' do
   context 'mission page' do
     before(:each) { visit about_mission_path }
-    scenario '' do
-      pending('Check for some text...')
-      fail
+    it 'has the appropriate header' do
+      expect(page).to have_text 'Mission'
     end
   end
   context 'policy page' do
     before(:each) { visit about_policy_path }
-    scenario '' do
-      pending('Check for some text...')
-      fail
+    it 'has the appropriate header' do
+      expect(page).to have_text 'Collection Development Policy'
     end
   end
   context 'partners and sponsors page' do
     before(:each) { visit about_partners_sponsors_path }
-    scenario '' do
-      pending('Check for some text...')
-      fail
+    it 'has the appropriate header' do
+      expect(page).to have_text 'DLG Partners and Sponsors'
     end
   end
   context 'api page' do
     before(:each) { visit about_api_path }
-    scenario '' do
-      pending('Check for some text...')
-      fail
+    it 'has the appropriate header' do
+      expect(page).to have_text 'DLG API'
     end
   end
 end
--- spec/features/participate_feature_spec.rb
+++ spec/features/participate_feature_spec.rb
@@ -5,23 +5,26 @@ require 'rails_helper'
 feature 'Participate' do
   context 'contribute page' do
     before(:each) { visit participate_contribute_path }
-    scenario '' do
-      pending('Check for some text...')
-      fail
+    it 'has the appropriate header' do
+      expect(page).to have_text 'Contribute Your Collections'
     end
   end
   context 'nominate page' do
     before(:each) { visit participate_nominate_path }
-    scenario '' do
-      pending('Check for a form element, a button, or some text...')
-      fail
+    it 'has the appropriate header' do
+      expect(page).to have_text 'Nominate Collections'
+    end
+    it 'has a Collection Name field' do
+      expect(page).to have_field 'Collection Name'
+    end
+    it 'has a Submit Nomination button' do
+      expect(page).to have_button 'Submit Nomination'
     end
   end
   context 'services page' do
     before(:each) { visit participate_partner_services_path }
-    scenario '' do
-      pending('Check for some text...')
-      fail
+    it 'has the appropriate header' do
+      expect(page).to have_text 'Services For Partners'
     end
   end
 end
