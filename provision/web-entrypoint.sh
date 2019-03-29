b1eaf695aeca4bd8416dba176c11c6f965ea2e35
Kevin <kevin.cottrell@uga.edu>
2019-03-29 16:54:59 -0400

    Update web-entrypoint.sh

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-groupadd -g 999 gitlab-runner && useradd -u 999 -g 999 gitlab-runner
+groupadd -g 999 gitlab-runner && useradd -u 999 -g 999 -d . gitlab-runner
 apt-get update -qq && apt-get -y install nodejs sudo
-sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
-sudo -u gitlab-runner bundle install
-sudo -u gitlab-runner bundle exec rake db:setup RAILS_ENV=test
-sudo -u gitlab-runner bundle exec rspec --color --format documentation
+bundle install
+bundle exec rake db:setup RAILS_ENV=test
+sudo -E -u bundle exec rspec --color --format documentation
