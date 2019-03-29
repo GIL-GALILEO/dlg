221b25335b5bb2dc7909ff571825c150d30de463
Kevin <kevin.cottrell@uga.edu>
2019-03-29 16:42:11 -0400

    Update .gitlab-ci.yml, provision/web-entrypoint.sh files

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
-cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
+groupadd -g 999 gitlab-runner && useradd -u 999 -g 999 gitlab-runner
 apt-get update -qq && apt-get -y install nodejs
+sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
 bundle install
 bundle exec rake db:setup RAILS_ENV=test
-bundle exec rspec --color --format documentation
+sudo -u gitlab-runner bundle exec rspec --color --format documentation
