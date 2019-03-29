91453538e26614b97a9f1fbbbac0720b58116ba2
Kevin <kevin.cottrell@uga.edu>
2019-03-29 17:01:55 -0400

    Upderp web-entrypoint.sh

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-groupadd -g 999 gitlab-runner && useradd -u 999 -g 999 -d . gitlab-runner
+groupadd -g 999 gitlab-runner && useradd -u 999 -g 999 gitlab-runner
 apt-get update -qq && apt-get -y install nodejs sudo
 sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
