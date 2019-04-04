37a740321d5364ac3aba11c66c882acc6ba865f2
Kevin <kevin.cottrell@uga.edu>
2019-04-04 15:05:35 -0400

    get crazy with environment variables

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -4,6 +4,6 @@ apt-get update -qq && apt-get -y install nodejs sudo
 sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
-HOME=/code bundle install
-HOME=/code bundle exec rake db:setup RAILS_ENV=test
+sudo -u gitlab-runner bundle install --path vendor/bundle
+PATH=`pwd`/vendor/bundle/ruby/2.4.0/bin:$PATH sudo -u gitlab-runner bundle exec rake db:setup RAILS_ENV=test
 sudo -E -u gitlab-runner bundle exec rspec --color --format documentation
