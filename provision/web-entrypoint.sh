1b0c4a733e5aef25ea7107dafde1a0816bbe27be
Kevin <kevin.cottrell@uga.edu>
2019-04-04 15:13:28 -0400

    Update web-entrypoint.sh, confirming path

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -5,6 +5,6 @@ sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloa
 sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
 sudo -u gitlab-runner bundle install --path vendor/bundle
-echo PATH=`pwd`/vendor/bundle/ruby/2.4.0/bin:$PATH
+ls -l /code/vendor/bundle/ruby/
 PATH=`pwd`/vendor/bundle/ruby/2.4.0/bin:$PATH sudo -u gitlab-runner bundle exec rake db:setup RAILS_ENV=test
 sudo -E -u gitlab-runner bundle exec rspec --color --format documentation
