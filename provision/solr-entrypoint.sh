e9e5014a4b1fa11a71ce3e2dc11b50af2e08616d
Mike Kanning <mikekanning@gmail.com>
2019-04-04 12:11:06 -0400

    remove lines that create dev solr collection. try a different strategy for indexing sample data.

--- provision/solr-entrypoint.sh
+++ provision/solr-entrypoint.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 mkdir /opt/solr/server/solr/configsets/meta
 mkdir /opt/solr/server/solr/configsets/meta/conf
 mkdir /opt/solr/server/solr/configsets/meta/conf/lang
@@ -8,8 +10,7 @@ cp /opt/solr/server/solr/configsets/basic_configs/conf/lang/stopwords_en.txt /op
 cp /opt/solr/server/solr/configsets/basic_configs/conf/synonyms.txt /opt/solr/server/solr/configsets/meta/conf/synonyms.txt
 cp /opt/solr/server/solr/configsets/basic_configs/conf/protwords.txt /opt/solr/server/solr/configsets/meta/conf/protwords.txt
 /opt/solr/bin/solr start -p 8983
-/opt/solr/bin/solr create_core -c meta-dev -d meta -p 8983
 /opt/solr/bin/solr create_core -c meta-test -d meta -p 8983
-/data/load_solr_data.sh
+curl -X POST 'http://solr:8983/solr/meta-test/update/json?commit=true' --data-binary @data.json -H 'Content-type:application/json'
 /opt/solr/bin/solr stop
 solr-foreground
