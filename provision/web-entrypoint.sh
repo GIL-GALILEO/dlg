3803ca6f154c62fa80310044ae23ef3652035158
Kevin <kevin.cottrell@uga.edu>
2019-04-04 14:55:09 -0400

    modify homedirs, what could go wrong?

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-groupadd -g 999 gitlab-runner && useradd -u 999 -g 999 gitlab-runner
+groupadd -g 999 gitlab-runner && useradd -u 999 -g 999 gitlab-runner && usermod -d /code gitlab-runner
 apt-get update -qq && apt-get -y install nodejs sudo
 sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
-bundle install
-bundle exec rake db:setup RAILS_ENV=test
+HOME=/code bundle install
+HOME=/code bundle exec rake db:setup RAILS_ENV=test
 sudo -E -u gitlab-runner bundle exec rspec --color --format documentation
