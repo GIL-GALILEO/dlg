d82c257df052636c9f28eb0041368e792227c773
Mike Kanning <mikekanning@gmail.com>
2019-06-27 11:36:15 -0400

    draft collection_resource controller action and view

--- app/controllers/collection_resources_controller.rb
+++ app/controllers/collection_resources_controller.rb
@@ -3,6 +3,8 @@
 # Collection Resource controller
 class CollectionResourcesController < HomepageController
   def show
-
+    collection_data = MetaApiV2.collection(params[:collection_record_id])
+    # TODO: this is way too crufty...
+    @page_content = collection_data['collection_resource'][params['collection_resource_slug']]
   end
 end
