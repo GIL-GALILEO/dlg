2231b25e9b676effd647cb5b9495e8df42f9c8d2
Mike Kanning <mikekanning@gmail.com>
2019-06-28 12:27:36 -0400

    modify and copy solr data seeding script to separate CI and Dev functionality

--- provision/load_solr_data.sh
+++ provision/load_solr_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-curl http://solr:8983/solr/meta-dev/update?commit=true -d  '<delete><query>*:*</query></delete>'
+#curl http://solr:8983/solr/meta-dev/update?commit=true -d  '<delete><query>*:*</query></delete>'
 curl http://solr:8983/solr/meta-test/update?commit=true -d  '<delete><query>*:*</query></delete>'
 
-curl -X POST 'http://solr:8983/solr/meta-dev/update/json?commit=true' --data-binary @data.json -H 'Content-type:application/json'
+#curl -X POST 'http://solr:8983/solr/meta-dev/update/json?commit=true' --data-binary @data.json -H 'Content-type:application/json'
 curl -X POST 'http://solr:8983/solr/meta-test/update/json?commit=true' --data-binary @data.json -H 'Content-type:application/json'
