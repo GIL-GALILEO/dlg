f6cf177122462038764783ad3659ba2f6d62cab6
Mike Kanning <mikekanning@gmail.com>
2019-07-29 11:18:42 -0400

    return previous handler configuration - handle customization in app code

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -9,8 +9,9 @@ class CatalogController < ApplicationController
 
     # Default parameters to send to solr for all search-like requests.
     # See also SearchBuilder#processed_parameters
-    config.default_solr_params = { qt: 'public' }
+    config.default_solr_params = { qt: 'search' }
 
+    # TODO: facets are defined in query handler so this should not be needed
     # makes debugging solr calls easier
     config.add_facet_fields_to_solr_request!
 
