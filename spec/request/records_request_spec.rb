490172ecb040f67024912d29ea5f1fb8ee433b48
Mike Kanning <mikekanning@gmail.com>
2019-08-02 16:53:06 -0400

    fix ALL record_id search spec

--- spec/request/records_request_spec.rb
+++ spec/request/records_request_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'JSON Endpoint', type: :request do
       expect(body['response']['docs'].length).to eq 1
     end
     it 'returns record edm_is_shown_* values' do
-      get '/records.json?search_field=id&q=gaarchives_cmf_70'
+      get '/records.json?search_field=record_id&q=gaarchives_cmf_70'
       body = JSON.parse(response.body)
       expect(body['response']['docs'][0]).to have_key 'edm_is_shown_at'
       expect(body['response']['docs'][0]).to have_key 'edm_is_shown_by'
     end
     it 'returns multiple records by record_id' do
-      get '/records.json?search_field=id&q=gaarchives_cmf_70+OR+dlg_vsbg_jaj111'
+      get '/records.json?search_field=record_id&q=gaarchives_cmf_70+OR+dlg_vsbg_jaj111'
       body = JSON.parse(response.body)
       expect(body['response']['docs'].length).to eq 2
     end
