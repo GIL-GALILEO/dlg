4d849ee9998bc2c7441b29972c31aed8733f2882
Mike Kanning <mikekanning@gmail.com>
2019-10-27 11:16:24 -0400

    only add OSF handler is OSD is found

--- app/assets/javascripts/openseadragon.js
+++ app/assets/javascripts/openseadragon.js
@@ -3,11 +3,12 @@
 
 $(document).on('turbolinks:load', function() {
     var viewer = $('#Openseadragon1').data('osdViewer');
-    viewer.addHandler('page', function (data) {
-        var page = data.page;
-        document.getElementById('osd-page-number').innerHTML = ( page + 1 );
-        // TODO: update URL and add page param
-        // var newUrl = url.split("#")[0] + page;
-        // history.replaceState(null, null, newUrl);
+    if (viewer)
+        viewer.addHandler('page', function (data) {
+            var page = data.page;
+            document.getElementById('osd-page-number').innerHTML = ( page + 1 );
+            // TODO: update URL and add page param
+            // var newUrl = url.split("#")[0] + page;
+            // history.replaceState(null, null, newUrl);
     });
 });
