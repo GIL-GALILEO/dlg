c9f2b48dc9debe9805d514bbfb99dca9af67a890
Mike Kanning <mikekanning@gmail.com>
2020-01-02 15:52:22 -0500

    use ruby docker image for 2.5. include kevin's CI file updates.

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -4,7 +4,6 @@ apt-get update -qq && apt-get -y install nodejs sudo
 sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
-phantomjs -v
 HOME=/code gem install bundler
 HOME=/code bundle install
 cp /code/config/database.yml.ci /code/config/database.yml
