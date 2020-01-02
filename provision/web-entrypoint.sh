1d568453f04bebab0005c717ca5d8fa161b0c443
Mike Kanning <mikekanning@gmail.com>
2020-01-02 16:17:24 -0500

    try to address phantomjs/openssl bug issue

--- provision/web-entrypoint.sh
+++ provision/web-entrypoint.sh
@@ -4,6 +4,9 @@ apt-get update -qq && apt-get -y install nodejs sudo
 sudo -u gitlab-runner wget --quiet https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
 sudo -u gitlab-runner tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 cp phantomjs-2.1.1-linux-x86_64/bin/phantomjs /bin/
+# Workaround for phantomjs openssl issue
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=918727
+ENV OPENSSL_CONF=/dev/null
 phantomjs -v
 #HOME=/code gem install bundler
 #HOME=/code bundle install
