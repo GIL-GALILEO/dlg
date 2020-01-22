c3d88fa7c541edb7704dff364afd48411763c6f6
Mike Kanning <mikekanning@gmail.com>
2020-01-22 12:23:30 -0500

    oops, send actual fulltext instead of a symbol

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -90,7 +90,7 @@ class SolrDocument
   end
 
   def fulltext
-    self[:fulltext_texts]&.reject(&:blank?) ? [:fulltext_texts].first : nil
+    self[:fulltext_texts]&.reject(&:blank?) ? self[:fulltext_texts].first : nil
   end
 
   def iiif_ids
